dd05812602457d1645883f2efc5993575b27216d
Fuad modaresi rad <fuadmodaresirad@gmail.com>
2025-09-07 18:50:13 +0000

    Remove code editor from all except IDE page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import { Header } from '@/components/header';
 import { Hero } from '@/components/hero';
 import { PersonalizedLearningPath } from '@/components/personalized-learning-path';
 import { CourseCatalog } from '@/components/course-catalog';
-import { CodeEditorSection } from '@/components/code-editor-section';
 import { Community } from '@/components/community';
 import { ProgressTracking } from '@/components/progress-tracking';
 import { Testimonials } from '@/components/testimonials';
@@ -17,7 +16,6 @@ export default function Home() {
         <Hero />
         <PersonalizedLearningPath />
         <CourseCatalog />
-        <CodeEditorSection />
         <IdeSection />
         <ProgressTracking />
         <Community />
